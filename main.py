eab49c48dae86ee74b5f91b722a7a01e160783f9
chendada-8474 <raymond96383@gmail.com>
2023-07-19 18:06:48 +0800

    run return value

--- main.py
+++ main.py
@@ -11,11 +11,13 @@ def run(request):
     stock_data = StockData()
 
     if stock_data.is_date_dup(stocks_today.price_date):
-        return
+        return "%s stock prices exist" % stock_data.last_date
 
     stocks = stocks_today.get_prices(config["target_stocks"])
 
     stock_data.insert(stocks)
+
+    return "%s stock prices inserted" % stock_data.last_date
 
 
 if __name__ == "__main__":
